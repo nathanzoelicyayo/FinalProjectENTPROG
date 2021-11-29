35107dd03bdbe62e4cb9eecac7f5ce2aa1437cc3
Nathan Zoe Licyayo <nathanzoe.licyayo@benilde.edu.ph>
2021-11-29 14:52:20 +0800

    Created Relationship between Schedule Table and Asp Net Users

---
+++ Migrations/20211129064252_AddRelationshipUserIdScheduledUser.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalProjectENTPROG.Migrations
+{
+    public partial class AddRelationshipUserIdScheduledUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "Schedules",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                table: "Schedules",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_ApplicationUserId",
+                table: "Schedules",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_AspNetUsers_ApplicationUserId",
+                table: "Schedules",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_AspNetUsers_ApplicationUserId",
+                table: "Schedules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Schedules_ApplicationUserId",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Schedules");
+        }
+    }
+}
---
+++ Migrations/20211129064857_UpdatedScheduleTable.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalProjectENTPROG.Migrations
+{
+    public partial class UpdatedScheduleTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_AspNetUsers_ApplicationUserId",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduledUser",
+                table: "Schedules");
+
+            migrationBuilder.RenameColumn(
+                name: "ApplicationUserId",
+                table: "Schedules",
+                newName: "ScheduledUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Schedules_ApplicationUserId",
+                table: "Schedules",
+                newName: "IX_Schedules_ScheduledUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_AspNetUsers_ScheduledUserId",
+                table: "Schedules",
+                column: "ScheduledUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_AspNetUsers_ScheduledUserId",
+                table: "Schedules");
+
+            migrationBuilder.RenameColumn(
+                name: "ScheduledUserId",
+                table: "Schedules",
+                newName: "ApplicationUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Schedules_ScheduledUserId",
+                table: "Schedules",
+                newName: "IX_Schedules_ApplicationUserId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ScheduledUser",
+                table: "Schedules",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_AspNetUsers_ApplicationUserId",
+                table: "Schedules",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
